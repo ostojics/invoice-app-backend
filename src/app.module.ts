b2157206949321d1d9fb45d0742beaab1476d6cf
ostojics <ostojic.dev@gmail.com>
2023-06-07 16:04:27 +0200

    feat: add prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InvoiceModule } from './invoice/invoice.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [InvoiceModule],
+  imports: [InvoiceModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
