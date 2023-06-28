6571687ece03e07f263439f1b125b1ab3a430ccd
ostojics <ostojic.dev@gmail.com>
2023-06-28 19:14:49 -0400

    update cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://127.0.0.1:5173', methods: '*' });
+  app.enableCors({
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    methods: '*',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(8080);
 }
